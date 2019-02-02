06ed376a136a0d5696e27670e1cfb2fda7a2c6e6
Kirt Gittens <kirt@velotrak.com>
2019-02-02 17:55:11 -0500

    Clean up main function by creating parse_commandline_configuration

--- src/klb.c
+++ src/klb.c
@@ -11,6 +11,12 @@
 #include "server.h"
 #include "io_utils.h"
 
+struct server_configuration {
+  int frontend_port; // The port that the KLB server will run on
+  struct sockaddr_in *backends; // A list of server addresses we're load balancing between
+  int backend_count;
+};
+
 struct sockaddr_in * get_server_address(char hostname[], char port[]) {
   struct addrinfo * result;
   struct addrinfo hints;
@@ -29,21 +35,38 @@ struct sockaddr_in * get_server_address(char hostname[], char port[]) {
   return (struct sockaddr_in *) result->ai_addr;
 }
 
-int main(int argc, char *argv[]) {
-  int port = atoi(argv[1]);
+struct server_configuration parse_commandline_configuration(char* arguments[], int argument_count) {
+  struct server_configuration config;
+
+  config.frontend_port = atoi(arguments[1]); // the server port
+
+  // don't count the first 2 arguments as backends (executable name and server port)
+  // Every backend should have 2 associated arguments (host, port)
+  config.backend_count = (argument_count - 2) / 2;
 
   struct sockaddr_in *backends;
-  backends = malloc(sizeof(struct sockaddr_in) * (argc - 2));
+  config.backends = malloc(sizeof(struct sockaddr_in) * config.backend_count);
 
   int current_backend = 0;
-  int backend_count = 0;
-  while (current_backend < (argc - 2)) {
-    // Each argument contains a host and port pair
-    backends[current_backend] = *get_server_address(argv[current_backend + 2], argv[current_backend + 3]);
-    current_backend +=2;
-    backend_count +=1;
+  int current_argument = 2;
+  char* host;
+  char* port;
+
+  while (current_argument < argument_count) {
+    host = arguments[current_argument];
+    port = arguments[current_argument + 1];
+
+    config.backends[current_backend] = *get_server_address(host, port);
+    current_argument +=2;
+    current_backend +=1;
   }
 
-  run_server(port, backends, backend_count);
+  return config;
+}
+
+int main(int argc, char *argv[]) {
+  struct server_configuration config;
+  config = parse_commandline_configuration(argv, argc);
+  run_server(config.frontend_port, config.backends, config.backend_count);
   return 0;
 }
